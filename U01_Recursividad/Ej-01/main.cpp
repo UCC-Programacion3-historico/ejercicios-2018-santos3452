e80d9e690130ddf38856649c72904e7f405649ec
santos3452 <santos.virga9@gmail.com>
2019-08-14 10:47:05 -0300

    comit 2

--- U01_Recursividad/Ej-01/main.cpp
+++ U01_Recursividad/Ej-01/main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
-#include "potencia.h"
+
+    using namespace std;
+
+
+
+void mostrar(char *arr) {
+    if (arr[0] != '\0') {
+        mostrar(arr + 1);
+        cout << arr[0] << " ";
+    }
+}
+
 
 int main() {
-    std::cout << "Ejercicio 01/01\n" << std::endl;
+    char a[] = "Hola Mundo Lindo";
+    mostrar(a);
     return 0;
 }
+
+    int main() {
+        int a[5];
+        a[0] = 1;
+        a[1] = 3;
+        a[2] = 2;
+        a[3] = 3;
+        a[4] = 2;
+        cout << "Hay " << contar(a, 3) << " tres en el arreglo " << endl;
+        return 0;
+    }
+    return 0;
+}
+
+
+
+
